25d0a14381067df2b4cd8216a9f60b53131f16ae
anneauzier <anne.auziier@outlook.com>
2023-11-17 14:23:27 -0400

    add .gitignore

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,6 @@
 import Vapor
 
+// Content -> encode / decode facilmente estruturas codificáveis para mensagens HTTP
 struct DataRequest: Content {
     let date: String
 }
@@ -13,15 +14,12 @@ struct ErrorRequested: Content {
 }
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "Hello, world!"
-    }
-
+    // EventLoopFuture -> Referência a um valor que pode ainda não estar disponível.
     app.post("converter") { req -> EventLoopFuture<Response> in
         let requestData = try req.content.decode(DataRequest.self)
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/mm/yyyy"
-        
+
         guard let date = dateFormatter.date(from: requestData.date) else {
             let error = ErrorRequested(error: "Formato de data inválido. Use dd/mm/yyyy.")
             return error.encodeResponse(status: .badRequest, for: req)
